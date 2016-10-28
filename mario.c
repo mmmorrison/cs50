b76b5a235e18a1342c16bd9ce62e4065bfd7b73e
Maria <mmorrison1978@gmail.com>
2016-10-27 20:30:20 -0600

    finished mario

--- mario.c
+++ mario.c
@@ -1,6 +1,31 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 
-int main (void){
-  printf("test\n");
+int main(void)
+{
+    int height;
+
+    do{
+        printf("Height: ");
+        height = GetInt();
+        if (height == 0)
+        {
+            return 0;
+        }
+    }
+    while (height < 1 || height > 23);
+    //height loop
+    for (int i = 0; i < height; i++){
+        //spaces loop
+        for (int j = 0; j < height - i - 1; j++)
+        {
+            printf(" ");
+        }
+        //hashes loop
+        for (int k = 0; k < i + 2; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
