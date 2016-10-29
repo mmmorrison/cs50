707643ee52c13cbdea648a7b8d1e358175ebe15f
Maria <mmorrison1978@gmail.com>
2016-10-28 21:24:43 -0600

    added do/while loop

--- greedy.c
+++ greedy.c
@@ -3,6 +3,12 @@
 #include <math.h>
 
 int int main(void) {
-  
+  float change;
+  int coins = 0;
+
+  do {
+    printf("How much change is owed? \n");
+    change = GetFloat();
+  } while(change <= 0);
 
 }
