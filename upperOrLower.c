0ccc182856a61ad7eec03931859173a250bf34d2
Maria <mmorrison1978@gmail.com>
2016-11-26 21:38:36 -0700

    added upperOrLower.c

---
+++ upperOrLower.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (int argc, string argv[])
+{
+    // Collect user input
+    printf("Please enter an alphabetical character: ");
+    char c = GetChar();
+
+    // If letter is uppercase
+    if (c <= 65 && c >= 90 )
+        printf("Thank you for the uppercase letter!\n");
+
+    // If the letter is lowercase
+    else if (c <= 97 && c >= 122)
+        printf("Thank you for the lowercase letter!\n");
+
+    // If not an alphabetical character
+    else
+        printf("You did not enter an alphabetical character!\n");
+};
