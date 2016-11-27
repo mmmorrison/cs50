08433ad5866c276beecc956cf0fab3c7982b7450
Maria <mmorrison1978@gmail.com>
2016-11-27 12:35:04 -0700

    added functions-0

---
+++ week2/functions-0.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) {
+
+  void PrintName(string, name)
+  {
+    printf("Hello, %s\n", name );
+  }
+
+  printf("Your name: \n", );
+  string s = GetString();
+  PrintName(s);
+}
