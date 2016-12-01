ce89e61af872436c91fa518fcf8b856ce00ab7a4
Maria <mmorrison1978@gmail.com>
2016-11-30 19:32:39 -0700

    fixed upperOrLower

--- study/upperOrLower.c
+++ study/upperOrLower.c
@@ -1,21 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main (int argc, string argv[])
+int main (void)
 {
     // Collect user input
     printf("Please enter an alphabetical character: ");
     char c = GetChar();
 
     // If letter is uppercase
-    if (c <= 'A' && c >= 'Z' )
+    if (c >= 65 && c <= 90 )
         printf("Thank you for the uppercase letter!\n");
 
     // If the letter is lowercase
-    else if (c <= 97 && c >= 122)
+    else if (c >= 97 && c <= 122)
         printf("Thank you for the lowercase letter!\n");
 
     // If not an alphabetical character
-    else
-        printf("You did not enter an alphabetical character!\n");
-};
+    // else
+    //     printf("You did not enter an alphabetical character!\n");
+}
