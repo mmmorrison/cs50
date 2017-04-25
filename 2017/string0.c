56df35062e4c6a34f8a253289d661a1435764a08
Maria <mmorrison1978@gmail.com>
2017-04-24 20:08:49 -0600

    updated

--- 2017/string0.c
+++ 2017/string0.c
@@ -1,8 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
-
+#include <string.h>
 
 int main(void) {
   string s = get_string();
-  printf("hello, %s\n", s);
+  int n = strlen(s);
+
+  for (int i = 0; i < n; i++)
+  {
+      printf("%c\n", s[i]);
+  }    
 }
